782a3353e7712dc1c5243fbab13248684ec75e25
ninlgde <ninlgde.ma@gmail.com>
2022-12-10 04:09:36 +0800

    [15] 支持if语句

--- src/keywords.rs
+++ src/keywords.rs
@@ -1,4 +1,6 @@
-pub const KEYWORDS: [&str; 1] = [KW_RETURN];
+pub const KEYWORDS: [&str; 3] = [KW_RETURN, KW_IF, KW_ELSE];
 
 /// 关键字: 返回
 pub const KW_RETURN: &str = "return";
+pub const KW_IF: &str = "if";
+pub const KW_ELSE: &str = "else";
