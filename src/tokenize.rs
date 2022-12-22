da5339abd75dd522276357aea86d5e59c17b3106
ninlgde <ninlgde.ma@gmail.com>
2022-12-22 13:47:06 +0800

    [77] 支持+= -= *= /=

--- src/tokenize.rs
+++ src/tokenize.rs
@@ -189,7 +189,7 @@ fn starts_with(chars: &Vec<u8>, pos: usize, sub: &str) -> bool {
 }
 
 fn read_punct(chars: &Vec<u8>, pos: &mut usize) {
-    let ops = vec!["==", "!=", "<=", ">=", "->"];
+    let ops = vec!["==", "!=", "<=", ">=", "->", "+=", "-=", "*=", "/="];
 
     for op in ops {
         if starts_with(chars, *pos, op) {
